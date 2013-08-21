6aaf8858d04452186ab101cf206ebbb534a76a2e
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-08-21 14:22:57 +0200

    register a layer

--- cs/blogextender/interfaces.py
+++ cs/blogextender/interfaces.py
@@ -1,6 +1,11 @@
 from zope.interface import Interface
 
 
+class ICSBlogExtenderLayer(Interface):
+    """A layer specific to my product
+    """
+
+
 class IBlog(Interface):
     """ Marker interface to blogs
     """
